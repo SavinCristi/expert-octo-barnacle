cc9efd9c9d887663ba188dd9810edc40c3b3d534
Cristi Savin <savinhouse@Cristis-MacBook-Pro.local>
2026-05-23 18:19:45 +0300

    v1.0 changes

--- backend/urls.py
+++ backend/urls.py
@@ -2,11 +2,13 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
+from django.views.generic import RedirectView
 
 from restaurant.views import event_inquiry, home, privacy_policy, reserve
 
 urlpatterns = [
     path("", home, name="home"),
+    path("meniu/", RedirectView.as_view(url="/?open=meniu"), name="meniu"),
     path("rezervare/", reserve, name="reserve"),
     path("eveniment/", event_inquiry, name="event_inquiry"),
     path("politica-de-confidentialitate/", privacy_policy, name="privacy_policy"),
